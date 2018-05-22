9e80c9d989da34a4a3170a84f95cc79be040e26a
Duc Le <leanhducvt@live.com>
2018-05-22 17:48:55 +0700

    Get pricing of all variants

--- cli.js
+++ cli.js
@@ -23,12 +23,16 @@ request(args[0], { json: true }, (err, res, html) => {
       const optionTag = $(li).find('a').children()[0];
       if (optionTag.name === 'img') {
         variantData.options.push({
+          optionId: `${$(child).find('ul').attr('data-sku-prop-id')}:${$(li).find('a').attr('data-sku-id')}`,
           src: optionTag.attribs.src,
           title: optionTag.attribs.title,
           bigpic: optionTag.attribs.bigpic,
         });
       } else if (optionTag.name === 'span') {
-        variantData.options.push($(optionTag).text());
+        variantData.options.push({
+          optionId: `${$(child).find('ul').attr('data-sku-prop-id')}:${$(li).find('a').attr('data-sku-id')}`,
+          text: $(optionTag).text(),
+        });
       }
     });
     data.variants.push(variantData);
@@ -37,6 +41,19 @@ request(args[0], { json: true }, (err, res, html) => {
   data.pics = [];
   $(html).find('.image-thumb-list').children().each((i, li) => {
     data.pics.push($(li).find('img').attr('src'));
+  })
+  // pricing
+  data.pricing = [];
+  const pricingJSON = /var.skuProducts=(.*);/.exec(html)[1];
+  const rawPricing = JSON.parse(pricingJSON);
+  rawPricing.map(price => {
+    // console.log(price)
+    data.pricing.push({
+      combinedOptions: price.skuAttr.match(/[0-9]{1,20}:[0-9]{1,20}/g),
+      singlePricing: price.skuVal.actSkuCalPrice,
+      bulkPricing: price.skuVal.actSkuBulkCalPrice,
+      bulkOrder: price.skuVal.bulkOrder,
+    })
   })
   console.log(JSON.stringify(data));
 
